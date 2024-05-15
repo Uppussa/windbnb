7b574e8a65bc864ea311c409a150932d8fb407e8
Jose Diaz <schmuckejgd@example.com>
2024-05-15 11:39:45 -0400

    Funcion completadas modifcar css

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,38 @@ import { useEffect, useState } from "react"
 import Card from './components/Card';
 import Nav from "./components/Nav";
 
-
 function App() {
   const [results, setResults] = useState([])
+  const [filtered, setFiltered] = useState([])
+
   const getData = async () => {
     const rs = await fetch('stays.json')
     const rsJson = await rs.json()
     setResults(rsJson)
+    setFiltered(rsJson) // No solo city, sino todos los resultados
   }
+
   useEffect(() => {
     getData()
   }, [])
 
-
   return (
-    <>
-      <div className="container">
-        <header>
-          <Nav />
-        </header>
-        <main>
-          <h2>Stays in Finland</h2>
-          <div className="listings">
-            {
-              results.map((result, index) => (
-              <Card key={index} result={result} />
-
-            ))}
-            
-
-          </div>
-        </main>
-
-        <footer>
-          <p>Created by username_here</p>
-        </footer>
-      </div>
-
-    </>
+    <div className="container">
+      <header>
+        <Nav results={results} setFiltered={setFiltered} />
+      </header>
+      <main>
+        <h2>Stays in Finland</h2>
+        <div className="listings">
+          {filtered.map((result, index) => (
+            <Card key={index} result={result} />
+          ))}
+        </div>
+      </main>
+      <footer>
+        <p>Created by username_here</p>
+      </footer>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,33 @@
+import { useState } from 'react'
+import logo from '../assets/logo.png'
 import './Nav.css'
 
+const Nav = ({ results, setFiltered }) => {
 
-const Nav = () => {
+  const [search, setSearch] = useState('')
+
+  function filterData() {
+    if (search.trim() !== '') {
+      const rs = results.filter(result => result.city.toLowerCase().includes(search.toLowerCase()))
+      setFiltered(rs)
+    } else {
+      setFiltered(results)
+    }
+  }
+
   return (
-    
-     <nav>
-      
-      <a href="#" className="logo">airbnb</a>
-      
+    <nav>
+      <a href="#" className="logo">
+        <img src={logo} alt="airbnb logo" className="logo-image" /> 
+      </a>
       <div className='inputs'>
-        <input className='locationInput' type="text" placeholder='LOCATION' />
+        <input className='locationInput' type="text" placeholder='LOCATION' value={search} onChange={(e) => setSearch(e.target.value)} />
         <input className='guestsInput' type="text" placeholder='Add Guests' />
-        <button>
-          <img src="#" alt="search"  />
+        <button onClick={filterData}>
+          <img src="#" alt="Buscar" />
         </button>
       </div>
-      </nav>
-  
-
+    </nav>
   )
 }
 
